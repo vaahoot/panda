5b0ba0c6548da9d1e77819672355939b584b47e1
Volodymyr Vakhotskyi <vaahoot@gmail.com>
2026-09-04 01:17:46 +0100

    lavalink password in .env

--- src/config/tokens.py
+++ src/config/tokens.py
@@ -9,3 +9,5 @@ ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 
 CR_API_KEY = os.getenv("CR_API_KEY")
 CR_API_HEADERS = {"Authorization": f"Bearer {CR_API_KEY}"}
+
+LAVALINK_PASSWORD = os.getenv("LAVALINK_PASSWORD", "password")
